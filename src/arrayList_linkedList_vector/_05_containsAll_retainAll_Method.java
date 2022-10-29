3e2e484713fe1f1c48e4506cf23c9c0dd1390fe0
Yoanna Matsevych <yana.matsevych@gmail.com>
2022-10-29 15:40:23 -0500

    added lines

---
+++ src/arrayList_linkedList_vector/_05_containsAll_retainAll_Method.java
@@ -0,0 +1,55 @@
+package arrayList_linkedList_vector;
+
+import java.util.ArrayList;
+
+public class _05_containsAll_retainAll_Method {
+    public static void main(String[] args) {
+
+        ArrayList<String> countries = new ArrayList<>();
+        countries.add("Italy");
+        countries.add("USA");
+        countries.add("Canada");
+        countries.add("Germany");
+        countries.add("Spain");
+        countries.add("Portugal");
+        countries.add("Sweden");
+
+        /*
+        Check if the countries Array list contains Spain
+         */
+
+        System.out.println(countries.contains("Spain"));
+
+         /*
+        Check if the countries Array list contains Sweden and Denmark
+            */
+
+        System.out.println(countries.contains("Sweden") && countries.contains("Denmark"));
+
+        boolean hasSwedan = false, hasDenmark = false;
+        for(String country : countries){
+            if(country.equals("Sweden")) hasSwedan = true;
+            else if (country.equals("Denmark")) hasDenmark = true;
+
+            if (hasSwedan && hasDenmark) break;
+
+        }
+
+        /*
+        Check if the countries Array list contains Sweden and Spain, Germany, Portugal
+            */
+        ArrayList<String> europeCountries = new ArrayList<>();
+        europeCountries.add("Sweden");
+        europeCountries.add("Portugal");
+        europeCountries.add("Spain");
+        europeCountries.add("Germany");
+        europeCountries.add("Italy");
+
+        System.out.println(countries.contains(europeCountries));
+
+
+        countries.retainAll(europeCountries);
+        System.out.println(countries);
+
+    }
+}
