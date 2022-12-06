788f4c6a6d58a8d572af23d21309614851dcf502
Yoanna Matsevych <yana.matsevych@gmail.com>
2022-12-06 17:46:36 -0600

    more solving for the homework

--- src/projects/Project08.java
+++ src/projects/Project08.java
@@ -16,12 +16,20 @@ public class Project08 {
         System.out.println(findClosestDistance(arr1_2));
         int[] arr1_3 = {10, -5, 20, 50, 100};
         System.out.println(findClosestDistance(arr1_3));
+        int[] arr1_4 = {4, 8, 7, 15};
+        System.out.println(findClosestDistance1(arr1_4));
 
         System.out.println("=============TASK-2============");
         int[] arr2_1 = {5, 3, -1, 3, 5, 7, -1};
         System.out.println(findSingleNumber(arr2_1));
-        int[] arr2_2 = {2};
+        int[] arr2_2 = {5, 3, -1, 3, 5, 7, 7};
         System.out.println(findSingleNumber(arr2_2));
+        int[] arr2_3 = {5, 3, -1, 3, 5, 4, -1};
+        System.out.println(findSingleNumber(arr2_3));
+        int[] arr2_4 = {2};
+        System.out.println(findSingleNumber(arr2_4));
+        int[] arr2_5 = {5, 3, -1, 3, 5, 7, -1};
+        System.out.println(findSingleNumber1(arr2_5));
 
         System.out.println("=============TASK-3===============");
         String str3 = "Hello";
@@ -77,6 +85,20 @@ public class Project08 {
         return difference;
     }
 
+    public static int findClosestDistance1(int[] arr) {
+        // look att current number, compare with all the number and find the smallest abs diff
+        // store that value and reassign it if needed
+        int len = arr.length, ans = MAX_VALUE;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int diff = Math.abs(arr[i] - arr[j]);
+                if (diff < ans) ans = diff;
+            }
+        }
+        return ans;
+    }
+
+
     /*
     TASK-2 – findSingleNumber() method
     Write a method that takes a int[] array as an argument and returns the
@@ -93,13 +115,24 @@ public class Project08 {
     7
      */
     public static int findSingleNumber(int[] arr){
+        Arrays.sort(arr);
+        if (arr.length == 1) return arr[0];
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i] != arr[i+1]) return arr[i];
+            else i++;
+        }
+        return arr[arr.length-1];
+    }
+
+    public static int findSingleNumber1(int[] arr){
+        if (arr.length == 1) return arr[0];
         int singleNumber = 0;
         ArrayList<Integer> hasDuplicate = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if(arr[i] == arr[j]) hasDuplicate.add(arr[i]);
-            }
-            if(!hasDuplicate.contains(arr[i])) singleNumber = arr[i];
+            if (!hasDuplicate.contains(arr[i])) {
+                hasDuplicate.add(arr[i]);
+                singleNumber += arr[i];
+            } else singleNumber -= arr[i];
         }
         return singleNumber;
     }
@@ -124,16 +157,15 @@ public class Project08 {
     Expected output 3:
     Space
      */
-    public static char findFirstUniqueCharacter(String str){
-        String hasDuplicate = "";
-       char[] arr = str.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) hasDuplicate += arr[j];
-            }
-            if (!hasDuplicate.contains(arr[i] + "")) return arr[i];
+    public static char findFirstUniqueCharacter(String str) {
+        while(str.length() > 0){
+            //if the character we are looking at, exist in the rest of the string
+            //remove all instances of that character, else return character
+            if(str.length() == 1) return str.charAt(0);
+            if(!str.substring(1).contains(str.charAt(0) + "")) return str.charAt(0);
+            else str = str.replaceAll("" + str.charAt(0), "");
         }
-            return ' ';
+        return ' ';
     }
 
     /*
@@ -164,5 +196,26 @@ public class Project08 {
             if (arr[i+1] - arr[i] != 1) missingNumber = arr[i] + 1;
         }
         return missingNumber;
+    }
+
+    public static int findMissingNumber1(int[] arr) {
+    // we eant to find min and max, and the range between those values is what to compare
+        int len = arr.length - 1, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 0, j = len; i <= len/2 ; i++, j--) {
+            if(arr[i] < min) min = arr[i];
+            if(arr[j] < min) min = arr[i];
+            if(arr[i] > max) max = arr[i];
+            if(arr[j] > min) max = arr[j];
+        }
+
+        //min = 1 and max = 5
+        //now that we have our range, we increment min++ and check if array contains that number
+        //to use contain method, we must make the array into a string
+
+        String s = Arrays.toString(arr).replace("]", ",");//[2, 3, 5, 6,
+        for (int i = min + 1; i < max; i++) {
+            if(!s.contains(i + ",")) return i;
+        }
+        return 0;
     }
 }
